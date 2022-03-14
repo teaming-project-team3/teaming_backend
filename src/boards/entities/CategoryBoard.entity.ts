dbee0b11ae07744ef20512928884d8e080022602
jeongminsu <ansuz0616@naver.com>
2022-03-14 18:17:18 +0900

    entity 구현
    일부 정리 필요

---
+++ src/boards/entities/CategoryBoard.entity.ts
@@ -0,0 +1,12 @@
+export class categoryBoard {
+  id: object;
+  title: string;
+  imgUrl: string;
+  contents: string;
+  stack: [string, number][];
+  nickname: string;
+  profileUrl: string;
+  period: Date;
+  likeCheck: boolean;
+  likeCount: number;
+}
