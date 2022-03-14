c650ddc24b1f8abf0b96c71c56cda6504589b0ce
jeongminsu <ansuz0616@naver.com>
2022-03-14 18:15:17 +0900

    log middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BoardsModule } from './boards/boards.module';
+import { logMiddleware } from './log.middleware';
+import { WebRtcModule } from './web-rtc/web-rtc.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@${process.env.MONGODB_KEY}:${process.env.MONGODB_PORT}`,
+      {
+        autoIndex: true,
+        ignoreUndefined: true,
+        dbName: 'db_nest',
+      },
+    ),
+    BoardsModule,
+    WebRtcModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logMiddleware).forRoutes('boards');
+  }
+}
