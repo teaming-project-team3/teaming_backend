b13296a68562af265491cd0673b73c2cc9475ed5
WON-JIN-LEE <vopgh0409@gmail.com>
2022-03-26 02:24:53 +0900

    fix: accept_join

--- src/chats/webrtc.gateway.ts
+++ src/chats/webrtc.gateway.ts
@@ -112,8 +112,8 @@ export class WebrtcGateway
     socket.join(data.roomName);
     console.log("after join, emit 'accept_join'", targetRoomObj.users);
 
-    this.server.to(data.roomName).emit('accept_join', targetRoomObj.users);
-    // socket.emit('accept_join', targetRoomObj.users);
+    // this.server.to(data.roomName).emit('accept_join', targetRoomObj.users);
+    socket.emit('accept_join', targetRoomObj.users);
     // socket.broadcast.to(data.roomName).emit('accept_join', targetRoomObj.users);
     //join_room end
   }
