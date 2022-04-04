a65900765184c62943c069208840af741058424d
WON-JIN-LEE <vopgh0409@gmail.com>
2022-04-04 15:43:02 +0900

    fix: cors 도메인 수정

--- src/chats/waitchats.gateway.ts
+++ src/chats/waitchats.gateway.ts
@@ -18,11 +18,7 @@ import { Chat } from 'src/schemas/Chat.schema';
 @WebSocketGateway({
   namespace: 'waitroom',
   cors: {
-    origin: [
-      'http://localhost:3000',
-      'http://teamingdeploy.s3-website.ap-northeast-2.amazonaws.com',
-      'https://wonjinlee.shop',
-    ],
+    origin: ['https://teaming.link', 'http://localhost:3000'],
     allowedHeaders: ['my-custom-header'],
     credentials: true,
   },
